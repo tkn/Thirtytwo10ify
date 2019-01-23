144449a922fccf387ac7bd8d24c6d14de7f89882
Torben Nielsen <github@torbennielsen.org>
2019-01-23 14:29:34 +0100

    Update 3210.js
    Fixed images not rendering due to size detection not waiting for images to load. Also fixed issue rendering scaled image.

--- web/3210.js
+++ web/3210.js
@@ -8,17 +8,22 @@ function onFileSelected(event) {
   imgtag.title = selectedFile.name;
 
   reader.onload = function(event) {
-    temp.src = event.target.result;
-    processImage(temp, imgtag, imgtag.parentNode.scrollWidth, imgtag.parentNode.scrollHeight, 1, true);
-	processImage(temp, pixelImg, pixelImg.parentNode.scrollWidth, pixelImg.parentNode.scrollHeight, 5, false);
-  };
+	temp.onload = function() {
+    	processImage(temp, imgtag, imgtag.parentNode.scrollWidth, imgtag.parentNode.scrollHeight, 1, true);
+		processImage(temp, pixelImg, pixelImg.parentNode.scrollWidth, pixelImg.parentNode.scrollHeight, 4, false);
+	}
+	temp.src = event.target.result;
+};
   
   reader.readAsDataURL(selectedFile);
+  
 }
 
 function processImage(src, dest, w, h, scale, smooth) {
-    var canvas = document.createElement("canvas");
-    var ctx = canvas.getContext("2d");
+	var canvas = document.createElement("canvas");
+	var canvasScaled = document.createElement("canvas");
+	var ctx = canvas.getContext("2d");
+	var ctxScaled = canvasScaled.getContext("2d");
     ctx.mozImageSmoothingEnabled = smooth;
 	ctx.webkitImageSmoothingEnabled = smooth;
 	ctx.imageSmoothingEnabled = smooth;
@@ -34,16 +39,16 @@ function processImage(src, dest, w, h, scale, smooth) {
 
 	var ratio = ratioW > ratioH ? ratioH : ratioW;
 
-	canvas.width = src.width * ratio
+	canvas.width = src.width * ratio;
 	canvas.height = src.height * ratio;
     ctx.drawImage(src, 0, 0, canvas.width, canvas.height);
-    
-    dest.src = canvas.toDataURL();
-    
-    if (scale > 1) {	    
-	    canvas.width *= scale;
-	    canvas.height *= scale;
-	    ctx.drawImage(dest, 0, 0, dest.width, dest.height, 0, 0, canvas.width, canvas.height);
-	    dest.src = canvas.toDataURL();
+	
+	if(scale != 1) {
+    	canvasScaled.width = canvas.width * scale;
+		canvasScaled.height = canvas.height * scale;
+		ctxScaled.drawImage(canvas, 0, 0, w, h, 0, 0, canvasScaled.width, canvasScaled.height);
+	} else {
+		canvasScaled = canvas;
 	}
+	dest.src = canvasScaled.toDataURL();
 }
